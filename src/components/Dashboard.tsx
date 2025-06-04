14853d8ff5f44be9c9082767219e9b42da473fdd
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-06-04 18:30:09 +0100

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -27,7 +27,7 @@ const Dashboard: React.FC = () => {
             <StatusCard 
               title="Deployment Status"
               status="success"
-              message="Congratulations Manual Build and Deployment of React Application is Successfull using Azure"
+              message="Congratulations Manual Build and Deployment of React Application is Successfull using GCP"
               timestamp={new Date().toLocaleString()}
             />
           </FadeIn>
