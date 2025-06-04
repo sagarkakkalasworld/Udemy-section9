140036e1c26c011a5b67d53199cb14232f4114c2
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-06-04 18:31:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/Udemy-section9/
 npm install
 npm run build
 chmod 744 push_docker_image.sh
-/home/ubuntu/sagar2/push_docker_image.sh
+/home/ubuntu/Udemy-section9/push_docker_image.sh
